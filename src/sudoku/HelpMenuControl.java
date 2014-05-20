468531decff778700e9bba20e3e69c32b05fbb3c
Higbee-Keith <macpro@10.0.1.39>
2014-05-19 19:25:30 -0700

    Merge origin/master
    Conflicts:
    	src/sudoku/Sudoku.java

---
+++ src/sudoku/HelpMenuControl.java
@@ -0,0 +1,46 @@
+package sudoku;
+/*
+Step 3:  Create another new class called HelpMenuControl. This class contains
+the commands to be executed for each menu item, and often referred to as the
+"control" layer because it controls what happens for each menu item selected.
+It must contain one or more functions that define the actions to be taken for
+each each menu item. 
+ */
+
+/**
+ *
+ * @author Keith Higbee, Jae Dillree, Josiah Hendricks, Jessie Gomez, Daniel Allen
+ */
+
+public class HelpMenuControl {
+    
+    //default constructor
+    public HelpMenuControl () {
+        
+    }
+    
+    //menu option method1
+    public void OptionOne() {
+        
+    }
+    
+    //menu option method2
+    public void OptionTwo() {
+        
+    }
+    
+    //menu option method3
+    public void OptionThree() {
+        
+    }
+    
+    //menu option method4
+    public void OptionFour() {
+        
+    }
+    
+    //menu option method5    
+    public void OptionFive() {
+        
+    }
+}
---
+++ src/sudoku/HelpMenuView.java
@@ -0,0 +1,44 @@
+package sudoku;
+/*
+Step 2: Create a new class for the “Help” menu and call it HelpMenuView. This
+class is responsible for displaying and retrieving the end user's input
+selection and is often referred to as the "view" layer.  It must contain:
+
+    *A function to display the help menu,
+    *A function to get the player's input selection
+    *A function to validate that the selection entered was valid. Use an
+    if-else-if ladder or a switch statement to determine if the selection
+    is valid. Use a do-while statement to loop until the end user (player) 
+    enters a valid selection.
+
+Step 4:  Modify the HelpMenuView class to call the appropriate action
+function in the HelpMenuControl class. 
+ */
+
+/**
+ *
+ * @author macpro
+ */
+public class HelpMenuView {
+
+    //create an array of Strings for the help menu items
+    
+    
+    //create instance of HelpMenuControl
+    
+    
+    //default constructor
+    public HelpMenuView() {
+        
+    }
+    
+    //call the display method and get user input
+    public void getInput() {
+        
+    }
+    
+    //displays the help menu
+    public void display() {
+        
+    }
+}
